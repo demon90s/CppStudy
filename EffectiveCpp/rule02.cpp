d9f88f387382d23c6cc1f09fdc411e7e25f19c44
demon90s <demon90s@163.com>
2018-06-25 12:13:49 -0400

    commit

---
+++ EffectiveCpp/rule02.cpp
@@ -0,0 +1,6 @@
+#include "rule02.h"
+
+int main()
+{
+	return 0;
+}
---
+++ EffectiveCpp/rule02.h
@@ -0,0 +1,9 @@
+// 条款2：尽量以const, enum, inline替换#define
+
+#define ASPECT_RATIO 1.653	// 宏定义不会计入记号表（symbol table）
+
+const double AspectRatio = 1.653;	// 使用常量替换上述宏
+
+const char* const authorName = "Scott Meyers"; // 定义常量char*-based字符串
+
+const std::string authorName2("Scott Meyers"); // 定义常量字符串更合宜的办法
