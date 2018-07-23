d8a09b64617e617874c0d852c7a03db6bc4077aa
demon90s <demon90s@163.com>
2018-07-23 19:38:21 -0400

    commit

---
+++ CppPrimer/ch12_Dynamic_Memory/example_TextQuery2/QueryResult.h
@@ -0,0 +1,32 @@
+#ifndef QUERY_RESULT_H
+#define QUERY_RESULT_H
+
+#include <iostream>
+#include <string>
+#include <set>
+#include <vector>
+#include <memory>
+#include "TextQuery.h"
+
+class TextQuery::QueryResult {
+	friend std::ostream& print(std::ostream&, const QueryResult&);
+public:
+	QueryResult(std::string s,
+				std::shared_ptr<std::set<line_no>> p,
+				std::shared_ptr<std::vector<std::string>> f):
+		sought(s), lines(p), file(f) {}
+
+	std::set<line_no>::iterator begin() { return lines->begin(); }
+	std::set<line_no>::iterator end()   { return lines->end(); }
+
+	std::shared_ptr<std::vector<std::string>> get_file() const { return file; }
+
+private:
+	std::string sought;	// 查询单词
+	std::shared_ptr<std::set<line_no>> lines;	// 出现的行号
+	std::shared_ptr<std::vector<std::string>> file;	// 输入文件
+};
+
+extern std::ostream& print(std::ostream&, const TextQuery::QueryResult&);
+
+#endif // QUERY_RESULT_H
---
+++ CppPrimer/ch19_Specialized_Tools_and_Techniques/example_20.md
@@ -0,0 +1,5 @@
+练习19.20：将你的QueryResult类嵌套在TextQuery中，然后重新运行12.3.2节（第435页）中使用了TextQuery的程序。
+
+---
+
+见：[新的TextQuery例程](../ch12_Dynamic_Memory/example_TextQuery2)
