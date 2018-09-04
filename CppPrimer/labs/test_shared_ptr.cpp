7f90f5eda78ad3194152cb1e77c93c7af2f85010
root <demon90s@163.com>
2018-09-04 18:37:17 +0800

    commit

--- CppPrimer/labs/test_shared_ptr.cpp
+++ CppPrimer/labs/test_shared_ptr.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int main()
+void test_use_count()
 {
 	auto p1 = make_shared<int>(10);
 	auto p2(p1);
@@ -12,6 +12,23 @@ int main()
 	cout << "p1 use_count: " << p1.use_count() << endl;
 	cout << "p2 use_count: " << p2.use_count() << endl;
 	cout << "p3 use_count: " << p3.use_count() << endl;
+}
+
+void test_deleter()
+{
+	auto deleter = [](int* p)
+	{
+		std::cout << "delete data: " << *p << std::endl;
+		delete p;
+	};
+
+	std::shared_ptr<int> p(new int(42), deleter);
+}
+
+int main()
+{
+	//test_use_count();
+	test_deleter();
 
 	return 0;
 }
