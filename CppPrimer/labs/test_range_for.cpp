af6bf3a5c739c835a94172c16fe51ab044470061
root <demon90s@163.com>
2018-09-22 13:08:34 +0800

    commit

---
+++ CppPrimer/labs/test_range_for.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+class IntArr {
+public:
+	explicit IntArr(int cnt) : cnt_(cnt) 
+	{ list_ = new int[10]{}; }
+
+	~IntArr()
+	{
+		delete[] list_;
+	}
+
+	int& operator[](int i)
+	{ return list_[i]; }
+
+	int* begin() { return list_; }
+	int* end() { return list_ + cnt_; }
+
+private:
+	int cnt_;
+	int *list_;
+};
+
+int main()
+{
+	{
+		IntArr int_arr(10);
+
+		for (auto &i : int_arr) {
+			i = 10;
+		}
+
+		for (auto i : int_arr) {
+			std::cout << i << " ";
+		}
+		std::cout << "\n";
+	}
+	
+	return 0;
+}
