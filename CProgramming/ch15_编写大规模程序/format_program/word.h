b2c2ab64dfe1c84f789c534c248f664796329396
root <demon90s@163.com>
2018-10-27 17:11:48 +0800

    commit

---
+++ CProgramming/ch15_编写大规模程序/format_program/word.h
@@ -0,0 +1,13 @@
+#ifndef WORD_H
+#define WORD_H
+
+/*
+ * read_word: Reads the next word from the input and
+ * 			  stores it in word. Makes word empty if no
+ * 			  word could be read because of end-of-file.
+ * 			  Truncates the word if its length exceeds len.
+ * 
+ */
+void read_word(char *word, int len);
+
+#endif
